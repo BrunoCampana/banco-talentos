081477d95b22bc57a4b162c4ee0d175f760e7e70
Bruno Campana <brunoramoscampana@gmail.com>
2019-09-12 09:56:02 -0400

    Removida a vulnerabilidade do diretorio raiz

--- config/routes.rb
+++ config/routes.rb
@@ -14,4 +14,5 @@ Rails.application.routes.draw do
   resources :cargoocupados
   resources :areaatuacaos
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "admin/dashboard#index"
 end
