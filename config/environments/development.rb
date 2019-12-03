b7cf63327a97941d5c02ec9547878b981cf1ba53
Bruno Campana <brunoramoscampana@gmail.com>
2019-12-03 10:22:26 -0400

    Insercao da linha de codigo que faltava

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,7 +73,8 @@ Rails.application.configure do
     authentication: "plain",
     enable_starttls_auto: true,
     user_name: ENV["MAIL_USERNAME"],
-    password: ENV["MAIL_PASSWORD"]
+    password: ENV["MAIL_PASSWORD"],
+    openssl_verify_mode: 'none'
   }
 
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
